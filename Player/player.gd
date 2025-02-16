bc03e8eaa19509af6fd32d4b46a421a28fa4e667
DrOtter3000 <info@darknet-online.com>
2025-02-16 18:21:51 +0100

    fix player collission position

--- Player/player.gd
+++ Player/player.gd
@@ -21,7 +21,6 @@ func _physics_process(delta: float) -> void:
 	velocity = direction * SPEED
 	
 	var mouse_position = get_global_mouse_position()
-	print(mouse_position)
 	if mouse_position.x > position.x:
 		sprite_2d.flip_h = false
 	if mouse_position.x < position.x:
