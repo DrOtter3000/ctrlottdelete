f2d4815474946aa6a857ed787dc36c03a6c9a360
DrOtter3000 <info@darknet-online.com>
2025-02-18 18:31:42 +0100

    add template level

--- Levels/level.gd
+++ Levels/level.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+@onready var ground: TextureRect = $Ground
+
+var floor_modulators := [Color.WHITE, Color.BEIGE, Color.BURLYWOOD, Color.DIM_GRAY]
+
+func _ready() -> void:
+	var modulator = floor_modulators.pick_random() as Color
+	ground.modulate = modulator
