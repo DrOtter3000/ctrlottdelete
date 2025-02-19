ce309b380e37dde901dc2627f2c2d626abb5b9cf
DrOtter3000 <info@darknet-online.com>
2025-02-19 18:55:18 +0100

    add pillar

--- Levels/Walls/wall_modular.gd
+++ Levels/Walls/wall_modular.gd
@@ -1,14 +1,25 @@
 extends StaticBody2D
 
+@export var wall := true
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 func update_style(style: String) -> void:
-	match style:
-		"blank":
-			sprite_2d.texture = load("res://Assets/Walls/wall base.png")
-		"vines":
-			sprite_2d.texture = load("res://Assets/Walls/wall vines.png")
-		"cracked":
-			sprite_2d.texture = load("res://Assets/Walls/wall cracked.png")
+	if wall:
+		match style:
+			"blank":
+				sprite_2d.texture = load("res://Assets/Walls/wall base.png")
+			"vines":
+				sprite_2d.texture = load("res://Assets/Walls/wall vines.png")
+			"cracked":
+				sprite_2d.texture = load("res://Assets/Walls/wall cracked.png")
+	else:
+		match style:
+			"blank":
+				sprite_2d.texture = load("res://Assets/Pillars/pillar base.png")
+			"vines":
+				sprite_2d.texture = load("res://Assets/Pillars/pillar vines.png")
+			"cracked":
+				sprite_2d.texture = load("res://Assets/Pillars/pillar cracked.png")
 	
 	
