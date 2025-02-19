5e9ac132c5432a35cf919d048c1ad692a9fded3a
DrOtter3000 <info@darknet-online.com>
2025-02-19 17:04:10 +0100

    make slime die

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 class_name Enemy
 
-@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 @onready var hurt_box: CollisionShape2D = $HurtBox
 @onready var attack_timer: Timer = $AttackTimer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -14,6 +13,7 @@ class_name Enemy
 
 var player
 var player_attackable := false
+var alive := true
 
 
 func _ready() -> void:
@@ -24,7 +24,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	player = get_tree().get_first_node_in_group("Player") as CharacterBody2D
 
-	if !player_attackable:
+	if !player_attackable and alive:
 		velocity = position.direction_to(player.position) * speed
 	else:
 		velocity = Vector2.ZERO
@@ -39,8 +39,18 @@ func _physics_process(delta: float) -> void:
 
 func hurt_player() -> void:
 	player.take_damage(damage)
-	check_if_alive()
 
+
+func take_damage(damage: float) -> void:
+	if alive:
+		#TODO: Animation
+		#TODO: Knockback
+		hitpoints -= damage
+		$Graphic/Sprite2D.modulate = Color.FUCHSIA
+		check_if_alive()
+		await get_tree().create_timer(.075).timeout 
+		$Graphic/Sprite2D.modulate = Color.WHITE
+		
 
 func check_if_alive() -> void:
 	if hitpoints <= 0:
@@ -48,16 +58,13 @@ func check_if_alive() -> void:
 
 
 func die() -> void:
-	queue_free()
-
-
-func take_damage(damage: float) -> void:
-	hitpoints -= damage
-	
+	alive = false
+	animation_player.play("die")
 
 
 func _on_attack_timer_timeout() -> void:
-	animation_player.play("attack")
+	if alive:
+		animation_player.play("attack")
 
 
 func _on_range_body_entered(body: Node2D) -> void:
