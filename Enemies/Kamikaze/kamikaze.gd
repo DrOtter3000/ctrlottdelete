5a4d44055664afee55e633e62169df851963493b
DrOtter3000 <info@darknet-online.com>
2025-02-21 18:27:10 +0100

    add kamikaze die animation

--- Enemies/Kamikaze/kamikaze.gd
+++ Enemies/Kamikaze/kamikaze.gd
@@ -26,3 +26,10 @@ func play_walk_sound() -> void:
 	
 	audio_stream_player_2d.pitch_scale = randf_range(.7, 1.4)
 	audio_stream_player_2d.play()
+
+
+func play_die_sound() -> void:
+	audio_stream_player_2d.stream = load("res://Audio/SFX/Enemies/can_die.wav")
+	audio_stream_player_2d.pitch_scale = randf_range(.7, 1.4)
+	audio_stream_player_2d.volume_db = 0
+	audio_stream_player_2d.play()
